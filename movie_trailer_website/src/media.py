e87ac83a633769794886caea6f9f1a7996998191
ravinder8ul <ravinder8ul@users.noreply.github.com>
2017-08-16 20:37:36 -0400

    made code PEP8 style compliant

--- movie_trailer_website/src/media.py
+++ movie_trailer_website/src/media.py
@@ -1,12 +1,26 @@
 import webbrowser
 
+
 class Movie():
+    """
+    Movie class stores movie properties and provides an API to show the movie trailer
+    """
     def __init__(self, movie_title, movie_storyline, poster_image,
                  trailer_youtube):
+        """
+        constructor initializes the following movie properties:
+        title of the movie
+        story line
+        url to the poster image
+        movie trailer link on youtube
+        """
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
 
     def show_trailer(self):
+        """
+        open the movie trailer in a webbrowser
+        """
         webbrowser.open(self.trailer_youtube_url)
--- movie_trailer_website/src/movie_trailer.py
+++ movie_trailer_website/src/movie_trailer.py
@@ -1,33 +1,41 @@
 import media
 import trailer_website
 
-#create instances of movies
-dangal = media.Movie("Dangal",
-                        "Former wrestler trains his daughters to follow in his footsteps",
-                        "https://upload.wikimedia.org/wikipedia/en/9/99/Dangal_Poster.jpg",
-                        "https://www.youtube.com/watch?v=x_7YlGv9u1g")
 
-bahubali2 = media.Movie("Baahubali2: The Conclusion",
-                        "Shiva, the son of Bahubali, begins to search for answers after he learns about his heritage",
-                        "https://upload.wikimedia.org/wikipedia/en/f/f9/Baahubali_the_Conclusion.jpg",
-                        "https://www.youtube.com/watch?v=G62HrubdD6o")
+# create instances of movies
+dangal = media.Movie(
+    "Dangal", "Former wrestler trains his daughters", "https://upload." +
+    "wikimedia.org/wikipedia/en/9/99/Dangal_Poster.jpg", "https://www." +
+    "youtube.com/watch?v=x_7YlGv9u1g"
+ )
 
-titanic = media.Movie("Titanic",
-                        "Epic movie on the ill-fated maiden voyage of the Titanic ship",
-                        "https://upload.wikimedia.org/wikipedia/en/2/22/Titanic_poster.jpg",
-                        "https://www.youtube.com/watch?v=2e-eXJ6HgkQ")
+bahubali2 = media.Movie(
+    "Baahubali2: The Conclusion", "Shiva, the son of Bahubali, begins to " +
+    "search for answers", "https://upload.wikimedia.org/wikipedia/en/f/f9/" +
+    "Baahubali_the_Conclusion.jpg", "https://www.youtube.com/watch?" +
+    "v=G62HrubdD6o"
+)
 
-avatar = media.Movie("Avataar",
-                     "A marine on an alien planet",
-                     "https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg",
-                     "https://www.youtube.com/watch?v=d1_JBMrrYw8")
+titanic = media.Movie(
+    "Titanic", "Epic movie on the ill-fated maiden voyage of the Titanic " +
+    "ship", "https://upload.wikimedia.org/wikipedia/en/2/22/Titanic_poster." +
+    "jpg", "https://www.youtube.com/watch?v=2e-eXJ6HgkQ"
+)
 
-pulp_fiction = media.Movie("Pulp Fiction",
-                     "Pulp Fiction",
-                     "https://upload.wikimedia.org/wikipedia/en/3/3b/Pulp_Fiction_%281994%29_poster.jpg",
-                     "https://www.youtube.com/watch?v=s7EdQ4FqbhY")
+avatar = media.Movie(
+    "Avataar", "A marine on an alien planet", "https://upload.wikimedia.org/" +
+    "wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg", "https://www.youtube.com/" +
+    "watch?v=d1_JBMrrYw8"
+)
 
-#add movie instances to a list
+pulp_fiction = media.Movie(
+    "Pulp Fiction", "Pulp Fiction", "https://upload.wikimedia.org/wikipedia/" +
+    "en/3/3b/Pulp_Fiction_%281994%29_poster.jpg", "https://www.youtube.com/" +
+    "watch?v=s7EdQ4FqbhY"
+)
+
+# add movie instances to a list
 movies = [avatar, dangal, pulp_fiction, titanic, bahubali2]
-#pass the movie list to the trailer website method that renders the movies to UI
+# pass the movie list to the trailer website method that
+# renders the movies to UI
 trailer_website.open_movies_page(movies)
